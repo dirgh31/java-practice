10d87676d93b6799e8fe5f27e8eebd631f1c3b4f
dirgh31 <128730815+dirgh31@users.noreply.github.com>
2026-05-05 19:19:23 +0530

    Practiced recursion: duplicates, subsequences, keypad, tower of hanoi, and related problems

---
+++ recursion/Recursion2Increasing.java
@@ -0,0 +1,16 @@
+public class Recursion2Increasing{
+    public static boolean isSorted (int arr[], int idx) {
+        if(idx == arr.length-1) {
+            return true;
+        }
+        if(arr[idx] >= arr[idx+1]) {
+            // araay is unsorted
+            return false;
+        } 
+        return isSorted(arr, idx+1);
+    }
+    public static void main(String[] args) {
+        int arr[] = {1, 3, 3};
+        System.out.println(isSorted(arr, 0));
+    }
+}
