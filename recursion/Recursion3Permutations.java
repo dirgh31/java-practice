02e5fb3c7babcd809d32d5182b34062813e29a07
dirgh31 <128730815+dirgh31@users.noreply.github.com>
2026-05-06 18:01:59 +0530

    Practiced recursion: permutations, subsets, total paths, place tiles, and invite pairing

---
+++ recursion/Recursion3Permutations.java
@@ -0,0 +1,22 @@
+public class Recursion3Permutations{
+    public static void printPermutations(String str, String permutation) {
+        if(str.length() == 0) {
+            System.out.println(permutation);
+            return;
+        }
+
+        for(int i = 0; i<str.length(); i++){
+            char currChar = str.charAt(i);
+            //"abc" -> "ab"
+            String newStr = str.substring(0, i) + str.substring(i+1);
+            printPermutations(newStr, permutation+currChar);
+
+        }
+
+    }
+    public static void main(String[] args) {
+        String str = "abc";
+        printPermutations(str, "");
+        
+    }
+}
---
+++ recursion/Recursion3Subsets.java
@@ -0,0 +1,33 @@
+
+import java.util.ArrayList;
+
+public class Recursion3Subsets{
+    public static void printSubset(ArrayList<Integer> subset) {
+        for(int i = 0; i<subset.size(); i++){
+            System.out.println(subset.get(i) + "");
+        }
+    }
+    public static void findSubsets(int n, ArrayList<Integer> subset) {
+
+        if(n == 0) {
+            printSubset(subset);
+            return;
+        }
+
+
+        //add
+        subset.add(n);
+        findSubsets(n-1, subset);
+
+        // cannot be added 
+        subset.remove(subset.size()-1);
+        findSubsets(n-1, subset);
+    }
+    public static void main(String[] args) {
+        int n = 3;
+        ArrayList<Integer> subset = new ArrayList<>();
+        findSubsets(n, subset);
+
+        
+    }
+}
