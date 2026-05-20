ab54fd6a4a608d0f507cd9728e5ed00bdb8be29a
dirgh31 <128730815+dirgh31@users.noreply.github.com>
2026-05-20 12:08:14 +0530

    Updated Bank class password methods

--- bank/Bank.java
+++ bank/Bank.java
@@ -1,6 +1,6 @@
 package bank;
 
-public class Bank{
+public class Bank {
     public static void main(String[] args) {
         Account account1 = new Account();
         account1.name = "Tony Stark";
@@ -8,6 +8,4 @@ public class Bank{
         account1.setPassword("abcd");
         System.out.println(account1.getPassword());
     }
-
 }
-
